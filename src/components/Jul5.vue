b99e8052a9122a14104a19c480634f8dc5f2bc2d
suzhengtang <1037617576@qq.com>
2018-07-06 00:54:03 +0800

    第一次提交

---
+++ src/components/Jul5.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="Jul5">
+      <p>{{data}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Jul5",
+      data(){
+          return {
+            data:'看完小程序20课，明天开始练习一晚！'
+          }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Jul3 from '@/components/Jul3'
 import Jul4 from '../components/Jul4'
+import Jul5 from '../components/Jul5'
+
 
 Vue.use(Router)
 
@@ -16,6 +18,11 @@ export default new Router({
       path: '/Jul4',
       name: 'Jul4',
       component: Jul4
+    },
+    {
+      path: '/Jul5',
+      name: 'Jul5',
+      component: Jul5
     }
   ]
 })
